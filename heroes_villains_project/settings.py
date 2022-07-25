2cf6cf139cc42435b4e40b9de53ed9c9cbcb9a83
JefferyBurgett <jeffery.burgett@gmail.com>
2022-07-25 18:53:57 -0500

    Added Project to Project Name

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -118,6 +118,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 try:
-    from heroes_villains.local_settings import *
+    from heroes_villains_project.local_settings import *
 except ImportError:
     raise ImportError("local settings not found")
---
+++ heroes_villains_project/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/supers/', include('supers.urls')),
+    path('api/super_types/', include('super_types.urls'))
+]
